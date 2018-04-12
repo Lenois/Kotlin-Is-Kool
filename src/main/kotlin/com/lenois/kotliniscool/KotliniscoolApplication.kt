93243e80c175664552ee2b730128e1ec9437b009
Evan John Gould <dcentauri@gmail.com>
2018-04-12 19:15:06 -0400

    kotlin

---
+++ src/main/kotlin/com/lenois/kotliniscool/KotliniscoolApplication.kt
@@ -0,0 +1,11 @@
+package com.lenois.kotliniscool
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class KotliniscoolApplication
+
+fun main(args: Array<String>) {
+    runApplication<KotliniscoolApplication>(*args)
+}
